2dce8f8055c993aa6e949b53bda1a047ebe20ec6
Wesley Toledo <wesley.toledo.hexatech@gmail.com>
2020-08-11 13:52:20 -0300

    Setup front-end folders and router

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Icon } from "tabler-react";
+
+function App() {
+  return (
+    <div className="App">
+      <Icon name="user" />
+    </div>
+  );
+}
+
+export default App;
